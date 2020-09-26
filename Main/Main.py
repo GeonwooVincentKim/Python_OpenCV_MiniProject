12a6f6a2a1fbb90705548869b7c258559552b2de
kdsnop@gmail.com <kdsnop@gmail.com>
2020-09-26 20:31:43 +0900

    Created saved file.

--- Main/Main.py
+++ Main/Main.py
@@ -1,10 +1,12 @@
 import cv2
 
 img_file = "../img/People.jpg"
+save_file = '../img/People_gary.jpg'
 img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
 
 if img is not None:
-    cv2.imshow("IMG", img)
+    cv2.imshow(img_file, img)
+    cv2.imwrite(save_file, img)
     cv2.waitKey()
     cv2.destroyAllWindows()
 else:
