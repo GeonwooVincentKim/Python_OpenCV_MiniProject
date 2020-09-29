c042c5dcc951d7de4ddfe39ff0edea9b0ef9f1e0
kdsnop@gmail.com <kdsnop@gmail.com>
2020-09-29 19:56:57 +0900

    Generated Image and video file-directory.

--- Main/Image/ImageFile.py
+++ Main/Image/ImageFile.py
@@ -1,7 +1,7 @@
 import cv2
 
-img_file = "../img/People.jpg"
-save_file = '../img/People_gary.jpg'
+img_file = "../../img/People.jpg"
+save_file = '../../img/People_gary.jpg'
 img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
 
 if img is not None:
