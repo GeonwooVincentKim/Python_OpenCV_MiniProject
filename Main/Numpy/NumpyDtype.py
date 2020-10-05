12f5ac274bb60bde361484bb9d8cc4d3a5054e38
kdsnop@gmail.com <kdsnop@gmail.com>
2020-10-05 21:37:54 +0900

    Added various reshape and arange attributes.

--- Main/Numpy/NumpyDtype.py
+++ Main/Numpy/NumpyDtype.py
@@ -7,3 +7,23 @@ print(b, a.dtype)
 
 c = a.astype(np.float64)
 print(c, c.dtype)
+
+print(a.dtype)
+
+d = np.uint8(a)
+print(d)
+
+a = np.arange(6)
+print(a)
+
+b = a.reshape(2, 3)
+print(b)
+
+c = a.reshape(a, (2, 3))
+print(c)
+
+d = np.arange(100).reshape(2, -1)
+print(d)
+
+e = np.arange(100).reshape(-1, 5)
+print(e, e.shape)
