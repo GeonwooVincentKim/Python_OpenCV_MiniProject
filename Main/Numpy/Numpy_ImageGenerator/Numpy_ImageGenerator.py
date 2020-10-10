08c721982d037583cd659081b35d0b0d32b08d00
kdsnop@gmail.com <kdsnop@gmail.com>
2020-10-10 20:51:37 +0900

    Generated one-dimensional Array and two-dimensional array.

---
+++ Main/Numpy/Numpy_ImageGenerator/Numpy_ImageGenerator.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+img = np.zeros((120, 120), dtype=np.uint8)
+img[25:35, :] = 45
+img[55:65, :] = 115
+img[85:95, :] = 160
+img[:, 35:45] = 205
+img[:, 75:85] = 255
+cv2.imshow('Gray', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
