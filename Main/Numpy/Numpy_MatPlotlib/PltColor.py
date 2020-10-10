335dc5cf7ea0d8d63310c1eb50432da184985496
kdsnop@gmail.com <kdsnop@gmail.com>
2020-10-10 21:46:00 +0900

    Successfully changed colors.

--- Main/Numpy/Numpy_MatPlotlib/PltColor.py
+++ Main/Numpy/Numpy_MatPlotlib/PltColor.py
@@ -3,5 +3,8 @@ import numpy as np
 
 x = np.arange(10)
 y = x ** 2
+fig, ax = plt.subplots(nrows=1, ncols=1)
 plt.plot(x, y, 'b')
+ax.set_facecolor('xkcd:black')
+ax.set_facecolor((1.0, 0.47, 0.42))
 plt.show()
