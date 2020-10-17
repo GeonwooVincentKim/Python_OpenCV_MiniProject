01a104c2dcb6601e97125358398df85d5f417ac4
kdsnop@gmail.com <kdsnop@gmail.com>
2020-10-17 21:06:06 +0900

    Back to original adding data section.

--- Main/ROI/ImageCalculation/ArithmeticMask.py
+++ Main/ROI/ImageCalculation/ArithmeticMask.py
@@ -3,7 +3,7 @@ import numpy as np
 
 a = np.array([[1, 2]], dtype=np.uint8)
 b = np.array([[10, 20]], dtype=np.uint8)
-mask = np.array([[1, 1]], dtype=np.uint8)
+mask = np.array([[1, 0]], dtype=np.uint8)
 
 cv1 = cv2.add(a, b, None, mask)
 print(cv1)
