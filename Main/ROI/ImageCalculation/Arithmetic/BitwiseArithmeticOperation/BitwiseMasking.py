13c040d006f0c3ddd6e12262ed03015343733782
kdsnop@gmail.com <kdsnop@gmail.com>
2020-10-18 21:23:47 +0900

    Show apart of image.

---
+++ Main/ROI/ImageCalculation/Arithmetic/BitwiseArithmeticOperation/BitwiseMasking.py
@@ -0,0 +1,16 @@
+import numpy as np, cv2
+import matplotlib.pylab as plt
+
+
+img = cv2.imread('../../../../../img/People.jpg')
+
+mask = np.zeros_like(img)
+cv2.circle(mask, (150, 140), 100, (255, 255, 255), -1)
+
+masked = cv2.bitwise_and(img, mask)
+
+cv2.imshow('originla', img)
+cv2.imshow('mask', mask)
+cv2.imshow('masked', masked)
+cv2.waitKey()
+cv2.destroyAllWindows()
