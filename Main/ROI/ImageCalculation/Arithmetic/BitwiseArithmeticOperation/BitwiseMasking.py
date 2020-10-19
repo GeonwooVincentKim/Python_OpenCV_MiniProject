f8b57d6b78faf0dfa8039cb5c99bbe99f50c5cb6
kdsnop@gmail.com <kdsnop@gmail.com>
2020-10-19 20:36:49 +0900

    Set the name right.

--- Main/ROI/ImageCalculation/Arithmetic/BitwiseArithmeticOperation/BitwiseMasking.py
+++ Main/ROI/ImageCalculation/Arithmetic/BitwiseArithmeticOperation/BitwiseMasking.py
@@ -9,7 +9,7 @@ cv2.circle(mask, (150, 140), 100, (255, 255, 255), -1)
 
 masked = cv2.bitwise_and(img, mask)
 
-cv2.imshow('originla', img)
+cv2.imshow('original', img)
 cv2.imshow('mask', mask)
 cv2.imshow('masked', masked)
 cv2.waitKey()
