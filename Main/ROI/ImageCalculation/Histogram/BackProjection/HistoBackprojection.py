5479d648b064dc7aadae91c0ccdf6bc2f788d3ba
kdsnop@gmail.com <kdsnop@gmail.com>
2020-10-22 22:10:33 +0900

    Remove Object Background which are cropped part by using Mouse.

--- Main/ROI/ImageCalculation/Histogram/BackProjection/HistoBackprojection.py
+++ Main/ROI/ImageCalculation/Histogram/BackProjection/HistoBackprojection.py
@@ -18,12 +18,34 @@ def masking(bp, win_name):
 
 
 def backProject_manual(hist_roi):
-    hist_img = cv2.calcHist([hsv_img], [0, 1], None, [100, 256], [0, 180, 0, 256])
+    hist_img = cv2.calcHist([hsv_img], [0, 1], None, [180, 256], [0, 180, 0, 256])
     hist_rate = hist_roi / (hist_img + 1)
+
     h, s, v = cv2.split(hsv_img)
+
     bp = hist_rate(h.ravel(), s.ravel())
     bp = np.minimum(bp, 1)
+
+    bp = bp.reshape(hsv_img.shape[2])
+    cv2.normalize(bp, bp, 0, 255, cv2.NORM_MINMAX)
+    bp = bp.astype(np.uint8)
     masking(bp, 'result_manual')
 
 
+def backProject_cv(hist_roi):
+    bp = cv2.calcBackProject([hsv_img], [0, 1], hist_roi, [0, 180, 0, 256], 1)
+    masking(bp, 'result_cv')
 
+
+(x, y, w, h) = cv2.selectROI(win_name, img, False)
+if w > 0 and h > 0:
+    roi = draw[y: y+h, x: x+w]
+    cv2.rectangle(draw, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+    hist_roi = cv2.calcHist([hsv_roi], [0, 1], None, [180, 256], [0, 180, 0, 256])
+    backProject_manual(hist_roi)
+    backProject_cv(hist_roi)
+
+cv2.imshow(win_name, draw)
+cv2.waitKey()
+cv2.destroyAllWindows()
