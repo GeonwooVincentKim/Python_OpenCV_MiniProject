49ab08cbff8c28437c8f1f0c490cb342fb7fb839
kdsnop@gmail.com <kdsnop@gmail.com>
2020-10-24 21:16:11 +0900

    Downloaded images from original Linkes.

---
+++ Main/ROI/ImageCalculation/Histogram/HistNormalizeEqualize/HistogramCompare/HistoCompare.py
@@ -0,0 +1,9 @@
+import cv2, numpy as np
+import matplotlib.pylab as plt
+
+
+img1 = cv2.imread('../../../../../../img/taekwon_v.jpg')
+img2 = cv2.imread('../../../../../../img/taekwonv1.jpg')
+img3 = cv2.imread('../../../../../../img/taekwonv2.jpg')
+img4 = cv2.imread('../../../../../../img/taekwonv3.jpg')
+
