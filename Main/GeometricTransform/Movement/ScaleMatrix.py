911391ccf543755b94a0bfea3e93cf6787b6ab7f
kdsnop@gmail.com <kdsnop@gmail.com>
2020-10-26 21:37:38 +0900

    Created variables named dst1 and dst2.

--- Main/GeometricTransform/Movement/ScaleMatrix.py
+++ Main/GeometricTransform/Movement/ScaleMatrix.py
@@ -4,3 +4,16 @@ import numpy as np
 
 img = cv2.imread('../../../img/fish.jpg')
 height, width = img.shape(2)
+
+m_small = np.float32([[0.5, 0, 0],
+                      [0, 0.5, 0]])
+m_big = np.float32([[2, 0, 0],
+                    [0, 2, 0]])
+
+dst1 = cv2.warpAffine(img, m_small,
+                      (int(height * 0.5),
+                       (int(width * 0.5))))
+dst2 = cv2.warpAffine(img, m_big,
+                      (int(height * 2),
+                       (int(width * 2))))
+
