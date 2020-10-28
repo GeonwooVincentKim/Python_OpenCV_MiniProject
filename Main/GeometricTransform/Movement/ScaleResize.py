217c97adf40cf78c0bd61a21323b9b2a608f75ee
kdsnop@gmail.com <kdsnop@gmail.com>
2020-10-28 22:08:53 +0900

    Expansion to Magnification-Point.

--- Main/GeometricTransform/Movement/ScaleResize.py
+++ Main/GeometricTransform/Movement/ScaleResize.py
@@ -5,8 +5,11 @@ import numpy as np
 img = cv2.imread('../../../img/fish.jpg')
 height, width = img.shape[:2]
 
+# Reduction to size-specification.
 dst1 = cv2.resize(img, (int(width * 0.5), int(height * 0.5)),
                   interpolation=cv2.INTER_AREA)
+
+# Expansion to Magnification-Point.
 dst2 = cv2.resize(img, None, 2, 2, cv2.INTER_CUBIC)
 
 cv2.imshow('original', img)
