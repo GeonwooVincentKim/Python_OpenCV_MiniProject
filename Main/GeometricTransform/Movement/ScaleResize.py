ca72df68f4f84bf05b27ec6b8a58fb69b705ff22
kdsnop@gmail.com <kdsnop@gmail.com>
2020-10-28 21:58:12 +0900

    Reduction to size-specification.

--- Main/GeometricTransform/Movement/ScaleResize.py
+++ Main/GeometricTransform/Movement/ScaleResize.py
@@ -4,3 +4,7 @@ import numpy as np
 
 img = cv2.imread('../../../img/fish.jpg')
 height, width = img.shape[:2]
+
+dst1 = cv2.resize(img, (int(width * 0.5), int(height * 0.5)),
+                  interpolation=cv2.INTER_AREA)
+dst2 = cv2.resize(img, None, 2, 2, cv2.INTER_CUBIC)
