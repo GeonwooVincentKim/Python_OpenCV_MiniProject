ae810e4e73e150ddaeb5d09c96c606151bada0d7
kdsnop@gmail.com <kdsnop@gmail.com>
2020-10-29 21:23:50 +0900

    Created four coordinate.

--- Main/GeometricTransform/Movement/Warping/Perspective.py
+++ Main/GeometricTransform/Movement/Warping/Perspective.py
@@ -5,3 +5,8 @@ import numpy as np
 file_name = "../../../../img/fish.jpg"
 img = cv2.imread(file_name)
 rows, cols = img.shape[:2]
+
+pts1 = np.float32([[0, 0], [0, rows], [cols, 0], [cols.rows]])
+pts2 = np.float32([[100, 50], [10, rows - 50], [cols - 100, 50], [cols - 10, rows - 50]])
+
+
