a8d168e02f096575864b220a58e00da35619bfa8
kdsnop@gmail.com <kdsnop@gmail.com>
2020-10-29 21:28:10 +0900

    Modified cols.rows as cols, rows.

--- Main/GeometricTransform/Movement/Warping/Perspective.py
+++ Main/GeometricTransform/Movement/Warping/Perspective.py
@@ -6,7 +6,7 @@ file_name = "../../../../img/fish.jpg"
 img = cv2.imread(file_name)
 rows, cols = img.shape[:2]
 
-pts1 = np.float32([[0, 0], [0, rows], [cols, 0], [cols.rows]])
+pts1 = np.float32([[0, 0], [0, rows], [cols, 0], [cols, rows]])
 pts2 = np.float32([[100, 50], [10, rows - 50], [cols - 100, 50], [cols - 10, rows - 50]])
 
 cv2.circle(img, (0, 0), 10, (255, 0, 0), -1)
