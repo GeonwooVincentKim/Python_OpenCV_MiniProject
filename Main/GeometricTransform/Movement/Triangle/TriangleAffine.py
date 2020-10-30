1cb94ae072ccb6d4420b78115725314bbc75b233
kdsnop@gmail.com <kdsnop@gmail.com>
2020-10-30 22:46:53 +0900

    Convert area of interest as a Triangle-Coordinates.

--- Main/GeometricTransform/Movement/Triangle/TriangleAffine.py
+++ Main/GeometricTransform/Movement/Triangle/TriangleAffine.py
@@ -14,3 +14,19 @@ x2, y2, w2, h2 = cv2.boundingRect(pts2)
 
 roi1 = img[y1: y1 + h1, x1: x1 + w1]
 roi2 = img[y2: y2 + h2, x2: x2 + w2]
+
+offset1 = np.zeros((3, 2), dtype=np.float32)
+offset2 = np.zeros((3, 2), dtype=np.float32)
+
+for i in range(3):
+    offset1[i][0], offset2[i][1] = pts1[i][0] - x1, pts2[i][1] - y1
+    offset1[i][0], offset2[i][1] = pts1[i][0] - x2, pts2[i][1] - y2
+
+
+mtrx = cv2.getAffineTransform(offset1, offset2)
+warped = cv2.warpAffine(roi1, mtrx, (w2, h2), None,
+                        cv2.INTER_LINEAR, cv2.BORDER_REFLECT_101)
+
+mask = np.zeros([h2, w2], dtype=np.uint8)
+cv2.fillConvexPoly(mask, np.int32(offset2), (255))
+
