065f680a3a0f30dd3b1578db7b3903a37893c187
kdsnop@gmail.com <kdsnop@gmail.com>
2020-10-30 22:50:49 +0900

    Display on windows.

--- Main/GeometricTransform/Movement/Triangle/TriangleAffine.py
+++ Main/GeometricTransform/Movement/Triangle/TriangleAffine.py
@@ -30,3 +30,16 @@ warped = cv2.warpAffine(roi1, mtrx, (w2, h2), None,
 mask = np.zeros([h2, w2], dtype=np.uint8)
 cv2.fillConvexPoly(mask, np.int32(offset2), (255))
 
+warped_masked = cv2.bitwise_and(warped, warped, mask=mask)
+roi2_masked = cv2.bitwise_and(roi2, roi2, mask=cv2.bitwise_not(mask))
+roi2_masked = roi2_masked + warped_masked
+img2[y2: y2 + h2, x2: x2 + w2] = roi2_masked
+
+cv2.rectangle(draw, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 0), 1)
+cv2.polylines(draw, [pts1.astype(np.int32)], True, (255, 0, 0), 1)
+cv2.rectangle(img2, (x2, y2), (x2 + w2, y2 + h2), (0, 255, 0), 1)
+
+cv2.imshow('origin', draw)
+cv2.imshow('warped triangle', img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
