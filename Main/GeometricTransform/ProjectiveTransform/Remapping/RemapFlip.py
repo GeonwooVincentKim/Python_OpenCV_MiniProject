2b712dd17cddf0406ef3262733b6634f28697d96
kdsnop@gmail.com <kdsnop@gmail.com>
2020-10-31 20:34:25 +0900

    Remapping the image as several images.

--- Main/GeometricTransform/ProjectiveTransform/Remapping/RemapFlip.py
+++ Main/GeometricTransform/ProjectiveTransform/Remapping/RemapFlip.py
@@ -10,3 +10,16 @@ mFlip = np.float32([[-1, 0, cols - 1], [0, -1, rows - 1]])
 flipped1 = cv2.warpAffine(img, mFlip, (cols, rows))
 print("Matrix: ", time.time() - st)
 
+st2 = time.time()
+mapY, mapX = np.indices((rows, cols), dtype=np.float32)
+mapX = cols - mapX - 1
+mapY = cols - mapY - 1
+flipped2 = cv2.remap(img, mapX, mapY, cv2.INTER_LINEAR)
+print("Remap: ", time.time() - st2)
+
+cv2.imshow("origin", img)
+cv2.imshow("flipped1", flipped1)
+cv2.imshow("flipped2", flipped2)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
