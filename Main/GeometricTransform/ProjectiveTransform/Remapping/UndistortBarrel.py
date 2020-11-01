7881e1f2eb4b8682fe22777945823405389af7fb
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-01 19:34:53 +0900

    Fixed some errors.

--- Main/GeometricTransform/ProjectiveTransform/Remapping/UndistortBarrel.py
+++ Main/GeometricTransform/ProjectiveTransform/Remapping/UndistortBarrel.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 
-
 img = np.full((300, 400, 3), 255, np.uint8)
 img[::10, :, :] = 0
 img[:, ::10, :] = 0
@@ -14,8 +13,8 @@ distCoeff = np.float64([k1, k2, p1, p2])
 # Set the random value of Camera Matrix
 fx, fy = 10, 10
 cx, cy = width / 2, height / 2
-camMatrix = np.float32([[fx, 0, cx,
-                         0, fy, cy],
+camMatrix = np.float32([[fx, 0, cx],
+                        [0, fy, cy],
                         [0, 0, 1]])
 
 dst = cv2.undistort(img, camMatrix, distCoeff)
