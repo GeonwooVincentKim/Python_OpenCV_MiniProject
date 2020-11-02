7c5efe1770efe6057e101eb500a9fc0edc48d41b
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-02 21:34:54 +0900

    Before transform of Coordinate of Triangle.

--- Main/GeometricTransform/Liquify/WorkshopLiquifyTool.py
+++ Main/GeometricTransform/Liquify/WorkshopLiquifyTool.py
@@ -1,18 +1,27 @@
-import cv2
-import numpy as np
+    import cv2
+    import numpy as np
 
 
-win_title = "Liquify"
-half = 50
-isDraggin = False
+    win_title = "Liquify"
+    half = 50
+    isDraggin = False
 
 
-def liquify(img, cx1, cy1, cx2, cy2):
-    x, y, w, h = cx1 - half, cy1 - half, half * 2, half * 2
-    roi = img[y: y + h, x: w + h].copy()
-    out = roi.copy()
+    def liquify(img, cx1, cy1, cx2, cy2):
+        x, y, w, h = cx1 - half, cy1 - half, half * 2, half * 2
+        roi = img[y: y + h, x: w + h].copy()
+        out = roi.copy()
 
-    offset_cx1, offset_cy1 = cx1 - x, cy1 - y
-    offset_cx2, offset_cy2 = cx2 - x, cy2 - y
+        offset_cx1, offset_cy1 = cx1 - x, cy1 - y
+        offset_cx2, offset_cy2 = cx2 - x, cy2 - y
 
+        tri1 = [[(0, 0), (w, 0), (offset_cx1, offset_cy1)],
+                [[0, 0], [0, h], [offset_cx1, offset_cy1]],
+                [[w, 0], [offset_cx1, offset_cy1], [w, h]],
+                [[0, h], [offset_cx1, offset_cy1], [w, h]]]
+
+        tri2 = [[(0, 0), (w, 0), (offset_cx2, offset_cy2)],
+                [[0, 0], [0, h], [offset_cx2, offset_cy2]],
+                [[w, 0], [offset_cx2, offset_cy2], [w, h]],
+                [[0, h], [offset_cx2, offset_cy2], [w, h]]]
 
