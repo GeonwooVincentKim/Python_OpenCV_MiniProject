1f25d9a20feffbe3728b36c0742c1d30104ea353
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-02 21:46:46 +0900

    Trying to feel differences..

--- Main/GeometricTransform/Liquify/WorkshopLiquifyTool.py
+++ Main/GeometricTransform/Liquify/WorkshopLiquifyTool.py
@@ -15,12 +15,12 @@ def liquify(img, cx1, cy1, cx2, cy2):
     offset_cx1, offset_cy1 = cx1 - x, cy1 - y
     offset_cx2, offset_cy2 = cx2 - x, cy2 - y
 
-    tri1 = [[(0, 0), (w, 0), (offset_cx1, offset_cy1)],
+    tri1 = [[[0, 0], [w, 0], [offset_cx1, offset_cy1]],
             [[0, 0], [0, h], [offset_cx1, offset_cy1]],
             [[w, 0], [offset_cx1, offset_cy1], [w, h]],
             [[0, h], [offset_cx1, offset_cy1], [w, h]]]
 
-    tri2 = [[(0, 0), (w, 0), (offset_cx2, offset_cy2)],
+    tri2 = [[[0, 0], [w, 0], [offset_cx2, offset_cy2]],
             [[0, 0], [0, h], [offset_cx2, offset_cy2]],
             [[w, 0], [offset_cx2, offset_cy2], [w, h]],
             [[0, h], [offset_cx2, offset_cy2], [w, h]]]
