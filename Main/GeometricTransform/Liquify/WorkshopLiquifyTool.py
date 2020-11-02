21718390393f2117816d1b0c3ed700317d0f55cd
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-02 22:08:09 +0900

    Trying to fine that cannot drag the mouse/

--- Main/GeometricTransform/Liquify/WorkshopLiquifyTool.py
+++ Main/GeometricTransform/Liquify/WorkshopLiquifyTool.py
@@ -9,13 +9,13 @@ isDragging = False
 
 def liquify(img, cx1, cy1, cx2, cy2):
     x, y, w, h = cx1 - half, cy1 - half, half * 2, half * 2
-    roi = img[y: y + h, x: w + h].copy()
+    roi = img[y: y + h, x: x + w].copy()
     out = roi.copy()
 
     offset_cx1, offset_cy1 = cx1 - x, cy1 - y
     offset_cx2, offset_cy2 = cx2 - x, cy2 - y
 
-    tri1 = [[[0, 0], [w, 0], [offset_cx1, offset_cy1]],
+    tri1 = [[(0, 0), (w, 0), (offset_cx1, offset_cy1)],
             [[0, 0], [0, h], [offset_cx1, offset_cy1]],
             [[w, 0], [offset_cx1, offset_cy1], [w, h]],
             [[0, h], [offset_cx1, offset_cy1], [w, h]]]
@@ -27,16 +27,21 @@ def liquify(img, cx1, cy1, cx2, cy2):
 
     for i in range(4):
         matrix = cv2.getAffineTransform(
-            np.float32(tri1[i]), np.float32(tri2[i])
+            np.float32(tri1[i]),
+            np.float32(tri2[i])
         )
         warped = cv2.warpAffine(
-            roi.copy(), matrix, (w, h),
-            None, flags=cv2.INTER_LINEAR,
+            roi.copy(),
+            matrix,
+            (w, h),
+            None,
+            flags=cv2.INTER_LINEAR,
             borderMode=cv2.BORDER_REFLECT_101
         )
         mask = np.zeros((h, w), dtype=np.uint8)
         cv2.fillConvexPoly(
-            mask, np.int32(tri2[i]),
+            mask,
+            np.int32(tri2[i]),
             (255, 255, 255)
         )
 
@@ -63,7 +68,7 @@ def onMouse(event, x, y, flags, param):
             cv2.rectangle(
                 img_draw,
                 (x - half, y - half),
-                (x - half, y - half),
+                (x + half, y + half),
                 (0, 255, 0)
             )
             cv2.imshow(win_title, img_draw)
