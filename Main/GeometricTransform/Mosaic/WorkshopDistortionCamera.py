25548286ab313b9c7688a35c329786e0101a80b1
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-04 22:06:01 +0900

    Modified some mistyped codes.

--- Main/GeometricTransform/Mosaic/WorkshopDistortionCamera.py
+++ Main/GeometricTransform/Mosaic/WorkshopDistortionCamera.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 rows, cols = 240, 320
-map_y, map_x = np.incides((rows, cols), dtype=np.float32)
+map_y, map_x = np.indices((rows, cols), dtype=np.float32)
 
 map_mirror_h_x, map_mirror_h_y = map_x.copy(), map_y.copy()
 map_mirror_v_x, map_mirror_v_y = map_x.copy(), map_y.copy()
