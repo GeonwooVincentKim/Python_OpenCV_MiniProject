ea07080a01a82c02bad1a7d23a3bd7c87c3fd139
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-08 20:54:27 +0900

    Applying Bluring..

--- Main/VideoFilter/ConvolutionFiltering/BlurAvgApi.py
+++ Main/VideoFilter/ConvolutionFiltering/BlurAvgApi.py
@@ -4,3 +4,8 @@ import numpy as np
 
 file_name = '../../../img/taekwonv1.jpg'
 img = cv2.imread(file_name)
+
+blur1 = cv2.blur(img, (10, 10))
+blur2 = cv2.boxFilter(img, -1, (10, 10))
+
+merged = np.hstack((img, blur1, blur2))
