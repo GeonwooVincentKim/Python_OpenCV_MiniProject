6d947bd3164e3ee74841ed7256212b26aed6c661
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-08 21:13:25 +0900

    Spacing codes.

--- Main/VideoFilter/ConvolutionFiltering/BlurAvgKernel.py
+++ Main/VideoFilter/ConvolutionFiltering/BlurAvgKernel.py
@@ -11,8 +11,8 @@ kernel = np.array([[0.04, 0.04, 0.04, 0.04, 0.04],
                    [0.04, 0.04, 0.04, 0.04, 0.04]])
 
 kernel = np.ones((5, 5)) / 5 ** 2
-
 blured = cv2.filter2D(img, -1, kernel)
+
 cv2.imshow('origin', img)
 cv2.imshow('avrg blur', blured)
 cv2.waitKey()
