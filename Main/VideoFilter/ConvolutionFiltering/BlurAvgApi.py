1de8c49a40ef8a661297af52f89f4c7dfcbc8cfc
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-08 20:55:01 +0900

    Applied BlurAvgApi.py file.:

--- Main/VideoFilter/ConvolutionFiltering/BlurAvgApi.py
+++ Main/VideoFilter/ConvolutionFiltering/BlurAvgApi.py
@@ -9,3 +9,6 @@ blur1 = cv2.blur(img, (10, 10))
 blur2 = cv2.boxFilter(img, -1, (10, 10))
 
 merged = np.hstack((img, blur1, blur2))
+cv2.imshow('blur', merged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
