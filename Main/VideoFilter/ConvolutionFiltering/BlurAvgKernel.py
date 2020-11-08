c4181b610befca72435667b5f1b1739d517039b9
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-08 20:50:02 +0900

    Display the results.

--- Main/VideoFilter/ConvolutionFiltering/BlurAvgKernel.py
+++ Main/VideoFilter/ConvolutionFiltering/BlurAvgKernel.py
@@ -11,3 +11,9 @@ kernel = np.array([[0.04, 0.04, 0.04, 0.04, 0.04],
                    [0.04, 0.04, 0.04, 0.04, 0.04]])
 
 kernel = np.ones((5, 5)) / 5 ** 2
+
+blured = cv2.filter2D(img, -1, kernel)
+cv2.imshow('origin', img)
+cv2.imshow('avrg blur', blured)
+cv2.waitKey()
+cv2.destroyAllWindows()
