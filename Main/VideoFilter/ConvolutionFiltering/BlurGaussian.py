fdfcc67e0c31aeab02f2eccd3eace17621f9d6a0
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-09 22:02:04 +0900

    Before print the Data.

--- Main/VideoFilter/ConvolutionFiltering/BlurGaussian.py
+++ Main/VideoFilter/ConvolutionFiltering/BlurGaussian.py
@@ -4,3 +4,14 @@ import numpy as np
 
 img = cv2.imread("../../../img/gaussian_noise.jpg")
 
+k1 = np.array([[1, 2, 1],
+               [2, 4, 2],
+               [1, 2, 1]]) * (1 / 16)
+
+blur1 = cv2.filter2D(img, -1, k1)
+
+k2 = cv2.getGaussianKernel(3, 0)
+blur2 = cv2.filter2D(img, -1, k2 * k2.T)
+
+blur3 = cv2.GaussianBlur(img, (3, 3), 0)
+
