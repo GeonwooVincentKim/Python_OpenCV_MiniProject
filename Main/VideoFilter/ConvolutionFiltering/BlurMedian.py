2558dfe7c4937581f7a390db42ca598a9a06fb57
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-09 22:15:23 +0900

    MedianBluring..

--- Main/VideoFilter/ConvolutionFiltering/BlurMedian.py
+++ Main/VideoFilter/ConvolutionFiltering/BlurMedian.py
@@ -6,3 +6,7 @@ img = cv2.imread("../../../img/salt_pepper_noise.jpg")
 
 blur = cv2.medianBlur(img, 5)
 
+merged = np.hstack((img, blur))
+cv2.imshow("media", merged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
