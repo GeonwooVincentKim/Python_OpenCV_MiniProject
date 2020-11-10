a134ba97d64c57cd2d035c154f2c974bf85fdf57
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-10 21:14:19 +0900

    Display the result.

--- Main/VideoFilter/EdgeFiltering/EdgeDifferential.py
+++ Main/VideoFilter/EdgeFiltering/EdgeDifferential.py
@@ -9,3 +9,9 @@ gy_kernel = np.array([[-1], [1]])
 
 edge_gx = cv2.filter2D(img, -1, gx_kernel)
 edge_gy = cv2.filter2D(img, -1, gy_kernel)
+
+merged = np.hstack((img, edge_gx, edge_gy))
+
+cv2.imshow("edge", merged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
