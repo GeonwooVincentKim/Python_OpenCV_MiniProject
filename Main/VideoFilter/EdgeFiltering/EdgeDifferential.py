a1798d3de5f9694cbeef191ba23af544c354e11c
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-10 21:13:37 +0900

    Generated DifferentialKernel and Applied to the FIlter.

--- Main/VideoFilter/EdgeFiltering/EdgeDifferential.py
+++ Main/VideoFilter/EdgeFiltering/EdgeDifferential.py
@@ -3,3 +3,9 @@ import numpy as np
 
 
 img = cv2.imread("../../../img/sudoku.png")
+
+gx_kernel = np.array([[-1, 1]])
+gy_kernel = np.array([[-1], [1]])
+
+edge_gx = cv2.filter2D(img, -1, gx_kernel)
+edge_gy = cv2.filter2D(img, -1, gy_kernel)
