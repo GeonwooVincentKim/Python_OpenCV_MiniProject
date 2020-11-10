51de194176590a95ea374800c2a6018c8c83c461
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-10 20:50:58 +0900

    Display the results.:

--- Main/VideoFilter/ConvolutionFiltering/BlurBilateral.py
+++ Main/VideoFilter/ConvolutionFiltering/BlurBilateral.py
@@ -7,3 +7,8 @@ img = cv2.imread('../../../img/gaussian_noise.jpg')
 blur1 = cv2.GaussianBlur(img, (5, 5), 0)
 blur2 = cv2.bilateralFilter(img, 5, 75, 75)
 
+merged = np.hstack((img, blur2, blur2))
+
+cv2.imshow('bilateral', merged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
