66bcd32367f55c3cb371dee0882349863a41f3ff
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-13 21:35:15 +0900

    Applied Prewitt Kernel.

--- Main/VideoFilter/EdgeFiltering/EdgePrewitt.py
+++ Main/VideoFilter/EdgeFiltering/EdgePrewitt.py
@@ -7,3 +7,6 @@ img = cv2.imread(file_name)
 
 gx_k = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
 gy_k = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
+
+edge_gx = cv2.filter2D(img, -1, gx_k)
+edge_gy = cv2.filter2D(img, -1, gy_k)
