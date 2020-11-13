ddaf9c7ef1d81f0c43525b4913c6a13e65488402
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-13 21:36:04 +0900

    Show the results.

--- Main/VideoFilter/EdgeFiltering/EdgePrewitt.py
+++ Main/VideoFilter/EdgeFiltering/EdgePrewitt.py
@@ -10,3 +10,8 @@ gy_k = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
 
 edge_gx = cv2.filter2D(img, -1, gx_k)
 edge_gy = cv2.filter2D(img, -1, gy_k)
+
+merged = np.hstack((img, edge_gx, edge_gy, edge_gx + edge_gy))
+cv2.imshow("prewitt cross", merged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
