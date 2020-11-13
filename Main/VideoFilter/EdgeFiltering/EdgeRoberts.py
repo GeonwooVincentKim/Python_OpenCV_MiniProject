71e25e86373fbf4dc7054e731c331038eb977515
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-13 21:27:13 +0900

    Generate kernel and apply it.

--- Main/VideoFilter/EdgeFiltering/EdgeRoberts.py
+++ Main/VideoFilter/EdgeFiltering/EdgeRoberts.py
@@ -3,3 +3,10 @@ import numpy as np
 
 
 img = cv2.imread('../../../img/sudoku.png')
+
+gx_kernel = np.array([[1, 0], [0, -1]])
+gy_kernel = np.array([[0, 1], [-1, 0]])
+
+edge_gx = cv2.filter2D(img, -1, gx_kernel)
+edge_gy = cv2.filter2D(img, -1, gy_kernel)
+
