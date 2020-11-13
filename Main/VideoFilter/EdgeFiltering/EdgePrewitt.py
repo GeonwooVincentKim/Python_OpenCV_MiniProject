9073f81c8f7255d35ad4b4b4bea46cc73cc58f43
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-13 21:34:20 +0900

    Generated PrewittKernel.py file.

--- Main/VideoFilter/EdgeFiltering/EdgePrewitt.py
+++ Main/VideoFilter/EdgeFiltering/EdgePrewitt.py
@@ -4,3 +4,6 @@ import numpy as np
 
 file_name = "../../../img/sudoku.png"
 img = cv2.imread(file_name)
+
+gx_k = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+gy_k = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
