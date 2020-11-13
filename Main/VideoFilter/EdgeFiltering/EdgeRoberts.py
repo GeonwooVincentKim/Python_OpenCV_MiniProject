0a51213238871a2a7df3ae7ddf34787c0826fb19
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-13 21:28:07 +0900

    Show the results.

--- Main/VideoFilter/EdgeFiltering/EdgeRoberts.py
+++ Main/VideoFilter/EdgeFiltering/EdgeRoberts.py
@@ -10,3 +10,7 @@ gy_kernel = np.array([[0, 1], [-1, 0]])
 edge_gx = cv2.filter2D(img, -1, gx_kernel)
 edge_gy = cv2.filter2D(img, -1, gy_kernel)
 
+merged = np.hstack((img, edge_gx, edge_gy, edge_gx + edge_gy))
+cv2.imshow('roberts cross', merged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
