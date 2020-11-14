a22d3ce780755fde5be948f40a67cf934188fba5
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-14 22:26:42 +0900

    Detect the boundary by using SobelAPI.

--- Main/VideoFilter/EdgeFiltering/EdgeSobel.py
+++ Main/VideoFilter/EdgeFiltering/EdgeSobel.py
@@ -9,3 +9,6 @@ gy_k = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
 
 edge_gx = cv2.filter2D(img, -1, gx_k)
 edge_gy = cv2.filter2D(img, -1, gy_k)
+
+sobel_x = cv2.Sobel(img, -1, 1, 0, ksize=3)
+sobel_y = cv2.Sobel(img, -1, 0, 1, ksize=3)
