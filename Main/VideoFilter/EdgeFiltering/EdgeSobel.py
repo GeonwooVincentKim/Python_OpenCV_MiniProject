e0c21dc23c2c768659ec8ff4d00ef15b7a1fee57
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-14 22:25:01 +0900

    Generate SobelKernel.

--- Main/VideoFilter/EdgeFiltering/EdgeSobel.py
+++ Main/VideoFilter/EdgeFiltering/EdgeSobel.py
@@ -3,3 +3,9 @@ import numpy as np
 
 
 img = cv2.imread("../../../img/sudoku.jpg")
+
+gx_k = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+gy_k = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+
+edge_gx = cv2.filter2D(img, -1, gx_k)
+edge_gy = cv2.filter2D(img, -1, gy_k)
