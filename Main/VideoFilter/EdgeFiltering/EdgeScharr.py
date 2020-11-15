151a9f9cb99d138711a723e51ff282fed699d145
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-15 22:07:08 +0900

    Apply Scharr-Kernel.

--- Main/VideoFilter/EdgeFiltering/EdgeScharr.py
+++ Main/VideoFilter/EdgeFiltering/EdgeScharr.py
@@ -7,3 +7,5 @@ img = cv2.imread("../../../img/sudoku.png")
 gx_k = np.array([[-3, 0, 3], [-10, 0, 10], [-3, 0, 3]])
 gy_k = np.array([[-3, -10, -3], [0, 0, 0], [3, 10, 3]])
 
+edge_gx = cv2.filter2D(img, -1, gx_k)
+edge_gy = cv2.filter2D(img, -1, gy_k)
