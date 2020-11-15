dd985cddd3c17fdfcdbb5e36194b9ac8a07aac51
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-15 22:06:35 +0900

    Generate the Scharr-Kernel.

--- Main/VideoFilter/EdgeFiltering/EdgeScharr.py
+++ Main/VideoFilter/EdgeFiltering/EdgeScharr.py
@@ -3,3 +3,7 @@ import numpy as np
 
 
 img = cv2.imread("../../../img/sudoku.png")
+
+gx_k = np.array([[-3, 0, 3], [-10, 0, 10], [-3, 0, 3]])
+gy_k = np.array([[-3, -10, -3], [0, 0, 0], [3, 10, 3]])
+
