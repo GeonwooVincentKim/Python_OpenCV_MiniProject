ccd4972967da8a41ac45f5187aaa7f32c780f8c1
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-15 22:08:31 +0900

    Detect the boundary-line by using Scharr-Kernel.

--- Main/VideoFilter/EdgeFiltering/EdgeScharr.py
+++ Main/VideoFilter/EdgeFiltering/EdgeScharr.py
@@ -9,3 +9,6 @@ gy_k = np.array([[-3, -10, -3], [0, 0, 0], [3, 10, 3]])
 
 edge_gx = cv2.filter2D(img, -1, gx_k)
 edge_gy = cv2.filter2D(img, -1, gy_k)
+
+scharr_x = cv2.Scharr(img, -1, 1, 0)
+scharr_y = cv2.Scharr(img, -1, 0, 1)
