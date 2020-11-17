8600780d5417897a6c48e308a603f718f14b5db5
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-17 20:56:28 +0900

    Display the Laplacian codes.

--- Main/VideoFilter/EdgeFiltering/EdgeLaplacian.py
+++ Main/VideoFilter/EdgeFiltering/EdgeLaplacian.py
@@ -4,3 +4,8 @@ import numpy as np
 
 img = cv2.imread("../../../img/sudoku.png")
 edge = cv2.Laplacian(img, -1)
+
+merged = np.hstack((img, edge))
+cv2.imshow("Laplacian", merged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
