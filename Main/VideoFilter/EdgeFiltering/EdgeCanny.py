f0fc118ddc25f1e41d3186709c15ac96dd9a22d7
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-17 21:18:39 +0900

    Display the results.

--- Main/VideoFilter/EdgeFiltering/EdgeCanny.py
+++ Main/VideoFilter/EdgeFiltering/EdgeCanny.py
@@ -5,3 +5,8 @@ import numpy as np
 img = cv2.imread('../../../img/sudoku.png')
 
 edges = cv2.Canny(img, 100, 200)
+
+cv2.imshow("Original", img)
+cv2.imshow("Canny", edges)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
