950e59f551dd1623d088c92f7a4ae11ce9985d88
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-21 22:19:33 +0900

    Spacing between code blocks.

--- Main/VideoFilter/Morphology/MorphOpenClose.py
+++ Main/VideoFilter/Morphology/MorphOpenClose.py
@@ -6,5 +6,6 @@ img1 = cv2.imread("../../../img/morph_dot.png", cv2.IMREAD_GRAYSCALE)
 img2 = cv2.imread("../../../img/morph_hole.png", cv2.IMREAD_GRAYSCALE)
 
 k = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+
 opening = cv2.morphologyEx(img1, cv2.MORPH_OPEN, k)
 closing = cv2.morphologyEx(img2, cv2.MORPH_CLOSE, k)
