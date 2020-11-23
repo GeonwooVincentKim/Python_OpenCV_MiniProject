75a925a05ed2a6a61ee0c30f3e942da614273c9f
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-23 21:08:09 +0900

    Applied Gradient-Calculation

--- Main/VideoFilter/Morphology/MorphHat.py
+++ Main/VideoFilter/Morphology/MorphHat.py
@@ -3,3 +3,8 @@ import numpy as np
 
 
 img = cv2.imread("../../../img/moon_gray.jpg")
+
+k = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
+
+top_hat = cv2.morphologyEx(img, cv2.MORPH_TOPHAT, k)
+black_hat = cv2.morphologyEx(img, cv2.MORPH_TOPHAT, k)
