5ade51b10b98b2ce8753817e4a617638b01519fa
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-23 21:05:17 +0900

    Modified some text

--- Main/VideoFilter/Morphology/MorphGradient.py
+++ Main/VideoFilter/Morphology/MorphGradient.py
@@ -7,7 +7,7 @@ img = cv2.imread("../../../img/morphological.png")
 k = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 gradient = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, k)
 
-merged = np.hstacl((img, gradient))
+merged = np.hstack((img, gradient))
 
 cv2.imshow("gradient", merged)
 cv2.waitKey(0)
