2cc6f96e24c4b17bb0f88ac1420681a116e45af9
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-23 21:03:02 +0900

    Apply the graideint

--- Main/VideoFilter/Morphology/MorphGradient.py
+++ Main/VideoFilter/Morphology/MorphGradient.py
@@ -3,3 +3,6 @@ import numpy as np
 
 
 img = cv2.imread("../../../img/morphological.png")
+
+k = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+gradient = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, k)
