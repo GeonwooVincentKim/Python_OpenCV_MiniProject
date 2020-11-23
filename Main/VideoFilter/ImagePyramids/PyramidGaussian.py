02483691f684fef6b7612f32fe2040cc3d1067d0
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-23 21:36:58 +0900

    Display the results

--- Main/VideoFilter/ImagePyramids/PyramidGaussian.py
+++ Main/VideoFilter/ImagePyramids/PyramidGaussian.py
@@ -5,3 +5,10 @@ img = cv2.imread("../../../img/People.jpg")
 
 smaller = cv2.pyrDown(img)
 bigger = cv2.pyrUp(img)
+
+cv2.imshow("img", img)
+cv2.imshow("pyrDown", smaller)
+cv2.imshow("pyrUp", bigger)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
