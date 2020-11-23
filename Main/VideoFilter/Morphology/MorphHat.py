c5faefdcf8949216c8c994c26f88293668954541
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-23 21:09:03 +0900

    Display the results

--- Main/VideoFilter/Morphology/MorphHat.py
+++ Main/VideoFilter/Morphology/MorphHat.py
@@ -8,3 +8,8 @@ k = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
 
 top_hat = cv2.morphologyEx(img, cv2.MORPH_TOPHAT, k)
 black_hat = cv2.morphologyEx(img, cv2.MORPH_TOPHAT, k)
+
+merged = np.hstack((img, top_hat, black_hat))
+cv2.imshow("Top-Hat Black-Hat", merged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
