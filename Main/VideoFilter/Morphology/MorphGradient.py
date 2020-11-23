a5365f0086c64870311c4f7c7dcbac89357b0104
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-23 21:04:59 +0900

    Display the results.

--- Main/VideoFilter/Morphology/MorphGradient.py
+++ Main/VideoFilter/Morphology/MorphGradient.py
@@ -6,3 +6,9 @@ img = cv2.imread("../../../img/morphological.png")
 
 k = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 gradient = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, k)
+
+merged = np.hstacl((img, gradient))
+
+cv2.imshow("gradient", merged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
