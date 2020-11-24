230e2756c6d0ded3cbe17bd8c2cfd8e0d766e8f4
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-24 21:18:02 +0900

    Recover the images.

--- Main/VideoFilter/ImagePyramids/PyramidLaplacian.py
+++ Main/VideoFilter/ImagePyramids/PyramidLaplacian.py
@@ -4,3 +4,9 @@ import numpy as np
 
 img = cv2.imread("../../../img/taekwonv1.jpg")
 
+smaller = cv2.pyrDown(img)
+bigger = cv2.pyrUp(smaller)
+
+laplacian = cv2.subtract(img, bigger)
+restored = bigger + laplacian
+merged = np.hstack((img, laplacian, bigger, restored))
