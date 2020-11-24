488586f6c0fcf858cbb4ef8cc2aea15bea8f0324
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-24 21:27:35 +0900

    Set Area-of Interest

--- Main/VideoFilter/Workshop/WorkshopMosaic2.py
+++ Main/VideoFilter/Workshop/WorkshopMosaic2.py
@@ -7,3 +7,6 @@ img = cv2.imread("../../../img/taekwonv1.jpg")
 
 while True:
     x, y, w, h = cv2.selectROI(win_title, img, False)
+    if w > 0 and h > 0:
+        roi = img[y: y + h,  x: x + w]
+        roi = cv2.blur(roi, (ksize, ksize))
