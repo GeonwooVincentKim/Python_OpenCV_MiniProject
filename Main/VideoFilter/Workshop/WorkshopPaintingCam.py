83b87be9640e26d60e3aac29776d31fa9e30a2f8
kdsnop@gmail.com <kdsnop@gmail.com>
2020-11-30 21:07:27 +0900

    Arrange codes again

--- Main/VideoFilter/Workshop/WorkshopPaintingCam.py
+++ Main/VideoFilter/Workshop/WorkshopPaintingCam.py
@@ -6,9 +6,11 @@ cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
     ret, frame = cap.read()
-    frame = cv2.resize(frame, None,
-                       fx=0.5, fy=0.5,
-                       interpolation=cv2.INTER_AREA)
+    frame = cv2.resize(
+        frame, None,
+        fx=0.5, fy=0.5,
+        interpolation=cv2.INTER_AREA
+    )
 
     if cv2.waitKey(1) == 27:
         break
