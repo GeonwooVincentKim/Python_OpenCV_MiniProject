c33c805e25f0d8d49f7d710f7328caf68295444b
kdsnop@gmail.com <kdsnop@gmail.com>
2020-12-03 21:27:36 +0900

    Generated CntrHierarchy.py file

--- Main/Contour/CntrFind.py
+++ Main/Contour/CntrFind.py
@@ -8,17 +8,20 @@ img2 = img.copy()
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, imth_res = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY_INV)
 
-img2, contour, hierarchy = cv2.findContours(
-    imth_res, cv2.RETR_EXTERNAL,
+im2, contour, hierarchy = cv2.findContours(
+    imth_res,
+    cv2.RETR_EXTERNAL,
     cv2.CHAIN_APPROX_NONE
 )
 
-img2, contour2, hierarchy = cv2.findContours(
-    imth_res, cv2.RETR_EXTERNAL,
+im2, contour2, hierarchy = cv2.findContours(
+    imth_res,
+    cv2.RETR_EXTERNAL,
     cv2.CHAIN_APPROX_SIMPLE
 )
 
 print('Number of Figure : %d {%d}' % (len(contour), len(contour2)))
+
 cv2.drawContours(img, contour, -1, (0, 255, 0), 4)
 cv2.drawContours(img2, contour2, -1, (0, 255, 0), 4)
 
@@ -28,7 +31,7 @@ for i in contour:
 
 for i in contour2:
     for j in i:
-        cv2.circle(img, tuple(j[0]), 1, (255, 0, 0), -1)
+        cv2.circle(img2, tuple(j[0]), 1, (255, 0, 0), -1)
 
 cv2.imshow("CHAIN_APPROX_NONE", img)
 cv2.imshow("CHAIN_APPROX_SIMPLE", img2)
