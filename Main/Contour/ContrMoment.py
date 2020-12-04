f1e6bcd2af38232de4ff6f9aa1fc53bac2c55766
kdsnop@gmail.com <kdsnop@gmail.com>
2020-12-04 21:29:23 +0900

    Delete unnecessary parameter

--- Main/Contour/ContrMoment.py
+++ Main/Contour/ContrMoment.py
@@ -6,7 +6,7 @@ img = cv2.imread("../../img/shapes.png")
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 ret, th = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY_INV)
-img2, contours, hierachy = cv2.findContours(
+contours, hierachy = cv2.findContours(
     th, cv2.RETR_EXTERNAL,
     cv2.CHAIN_APPROX_SIMPLE
 )
