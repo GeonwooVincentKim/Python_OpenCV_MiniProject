523900506d9d18d870d7a449b5ffb02d1458df0e
kdsnop@gmail.com <kdsnop@gmail.com>
2020-12-05 19:57:23 +0900

    Convert to Binary Scale and Gray-Scale by read the images

--- Main/Contour/CntrBoundFit.py
+++ Main/Contour/CntrBoundFit.py
@@ -3,3 +3,6 @@ import numpy as np
 
 
 img = cv2.imread("../../img/lightning.png")
+
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, th = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY_INV)
