a35eac65c67b9afe9ca82bd2d91cc863235086b4
kdsnop@gmail.com <kdsnop@gmail.com>
2020-12-05 19:59:48 +0900

    Arranging codes

--- Main/Contour/CntrBoundFit.py
+++ Main/Contour/CntrBoundFit.py
@@ -3,6 +3,19 @@ import numpy as np
 
 
 img = cv2.imread("../../img/lightning.png")
+img_gray = cv2.cvtColor(
+    img, cv2.COLOR_BGR2GRAY
+)
 
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, th = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY_INV)
+ret, th = cv2.threshold(
+    img_gray,
+    127, 255,
+    cv2.THRESH_BINARY_INV
+)
+
+im, contours, hr = cv2.findContours(
+    th, cv2.RETR_EXTERNAL,
+    cv2.CHAIN_APPROX_SIMPLE
+)
+
+contr = contours[0]
