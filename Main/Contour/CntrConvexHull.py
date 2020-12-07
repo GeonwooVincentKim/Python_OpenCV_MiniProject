cd13d76804d61c23ab51a371df613771251dd8aa
kkwnop <kdsnop@gmail.com>
2020-12-07 21:55:14 +0900

    Downloaded right image

--- Main/Contour/CntrConvexHull.py
+++ Main/Contour/CntrConvexHull.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-img = cv2.imread("../../img/my_hand.jpg")
+img = cv2.imread("../../img/hand.jpg")
 img2 = img.copy()
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, th = cv2.threshold(
