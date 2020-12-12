009d72c4eab39c28fd509462ecc0aa2c03450c9f
kkwnop <kdsnop@gmail.com>
2020-12-12 21:11:26 +0900

    Fine the HoughLine

--- Main/ImageDivision/HoughTransform/HoughLine.py
+++ Main/ImageDivision/HoughTransform/HoughLine.py
@@ -5,3 +5,7 @@ import numpy as np
 img = cv2.imread("../../../img/sudoku.png")
 img2 = img.copy()
 h, w = img.shape[:2]
+
+imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(imgray, 100, 200)
+lines = cv2.HoughLines(edges, 1, np.pi / 180, 130)
