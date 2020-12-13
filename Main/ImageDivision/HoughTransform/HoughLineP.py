5a2cd10a578b4d4f5e889db936ebc17b1732b8f5
kkwnop <kdsnop@gmail.com>
2020-12-13 20:49:37 +0900

    Convert Probability of HurfConvertion

--- Main/ImageDivision/HoughTransform/HoughLineP.py
+++ Main/ImageDivision/HoughTransform/HoughLineP.py
@@ -7,3 +7,10 @@ img2 = img.copy()
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(imgray, 50, 200)
 
+lines = cv2.HoughLinesP(
+    edges,
+    1,
+    np.pi / 180,
+    10, None,
+    20, 2
+)
