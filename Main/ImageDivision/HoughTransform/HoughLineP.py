31c442817c699ac0cecef773f5deda95c480a516
kkwnop <kdsnop@gmail.com>
2020-12-13 20:58:36 +0900

    Display the line

--- Main/ImageDivision/HoughTransform/HoughLineP.py
+++ Main/ImageDivision/HoughTransform/HoughLineP.py
@@ -12,5 +12,20 @@ lines = cv2.HoughLinesP(
     1,
     np.pi / 180,
     10, None,
-    20, 2
+    20,
+    2
 )
+
+for line in lines:
+    x1, y1, x2, y2 = line[0]
+    cv2.line(
+        img,
+        (x1, y1),
+        (x2, y2),
+        (0, 255, 0),
+        1
+    )
+
+cv2.imshow("Probability Hough Line", img)
+cv2.waitKey()
+cv2.destroyAllWindows()
