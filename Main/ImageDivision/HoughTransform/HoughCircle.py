73545f2a8d584f2e041a5b51d089882cad911e52
kkwnop <kdsnop@gmail.com>
2020-12-17 21:25:36 +0900

    Convert as Gray-Scale

--- Main/ImageDivision/HoughTransform/HoughCircle.py
+++ Main/ImageDivision/HoughTransform/HoughCircle.py
@@ -3,3 +3,12 @@ import numpy as np
 
 
 img = cv2.imread("../../../img/coins_spread1.jpg")
+gray = cv2.cvtColor(
+    img,
+    cv2.COLOR_BGR2GRAY
+)
+blur = cv2.GaussianBlur(
+    gray,
+    (3, 3),
+    0
+)
