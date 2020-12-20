6482788a52c5cc94a8b9a1c88f4d3e2145609c9c
kkwnop <kdsnop@gmail.com>
2020-12-20 21:09:12 +0900

    Convert GrayScale and Binary Scale

--- Main/ImageDivision/DivisionContinousTerritory/ConnectedLabel.py
+++ Main/ImageDivision/DivisionContinousTerritory/ConnectedLabel.py
@@ -4,3 +4,14 @@ import numpy as np
 
 img = cv2.imread("../../../img/shapes_donut.png")
 img2 = np.zeros_like(img)
+
+gray = cv2.cvtColor(
+    img,
+    cv2.COLOR_BGR2GRAY
+)
+
+th = cv2.threshold(
+    gray,
+    127, 255,
+    cv2.THRESH_BINARY
+)
