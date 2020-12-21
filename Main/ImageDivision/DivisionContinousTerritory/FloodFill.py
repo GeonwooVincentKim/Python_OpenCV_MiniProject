3e3098f557b8765ae14ccd43c75c50b27ce7d3f2
kkwnop <kdsnop@gmail.com>
2020-12-21 21:48:59 +0900

    Declare variables for color or some variable for difference value between maximum and minimum:

--- Main/ImageDivision/DivisionContinousTerritory/FloodFill.py
+++ Main/ImageDivision/DivisionContinousTerritory/FloodFill.py
@@ -4,3 +4,13 @@ import numpy as np
 
 img = cv2.imread("../../../img/taekwonv1.jpg")
 rows, cols = img.shape[:2]
+mask = np.zeros(
+    (
+        rows + 2,
+        cols + 2
+    ),
+    np.uint8
+)
+
+newVal = (255, 255, 255)
+loDiff, upDiff = (10, 10, 10), (10, 10, 10)
