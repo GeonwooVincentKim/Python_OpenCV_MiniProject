3dea429c3938cf9f7b4dc36c5c5daac8b7ae1261
kkwnop <kdsnop@gmail.com>
2020-12-27 20:37:44 +0900

    MarkerID should be start from '1'

--- Main/ImageDivision/DivisionContinousTerritory/WaterShed.py
+++ Main/ImageDivision/DivisionContinousTerritory/WaterShed.py
@@ -5,3 +5,14 @@ import numpy as np
 img = cv2.imread("../../../img/taekwonv1.jpg")
 rows, cols = img.shape[:2]
 img_draw = img.copy()
+
+marker = np.zeros(
+    (
+        rows, cols
+    ),
+    np.int32
+)
+
+markerID = 1
+colors = []
+isDragging = False
