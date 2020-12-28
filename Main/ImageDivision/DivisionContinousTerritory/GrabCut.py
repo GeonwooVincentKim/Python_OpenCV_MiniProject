c8554b5ed50e753fb72a4736a7f655bfdc57aa99
kkwnop <kdsnop@gmail.com>
2020-12-28 21:16:02 +0900

    Create Mask and Buffer background and foreground model

--- Main/ImageDivision/DivisionContinousTerritory/GrabCut.py
+++ Main/ImageDivision/DivisionContinousTerritory/GrabCut.py
@@ -1,6 +1,15 @@
 import cv2
-import numpy as p
+import numpy as np
 
 
 img = cv2.imread("../../../img/taekwonv1.jpg")
 img_draw = img.copy()
+
+mask = np.zeros(
+    img.shape[:2],
+    dtype=np.uint8
+)
+rect = [0, 0, 0, 0]
+mode = cv2.GC_EVAL
+bgd_model = np.zeros((1, 65), np.float64)
+fgd_model = np.zeros((1, 65), np.float64)
