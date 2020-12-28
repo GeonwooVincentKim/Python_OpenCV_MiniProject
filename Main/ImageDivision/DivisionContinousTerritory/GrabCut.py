a73bf865e281e1abd93811afe64443442d26289e
kkwnop <kdsnop@gmail.com>
2020-12-28 21:17:06 +0900

    Generated Mouse-event-handler function

--- Main/ImageDivision/DivisionContinousTerritory/GrabCut.py
+++ Main/ImageDivision/DivisionContinousTerritory/GrabCut.py
@@ -13,3 +13,11 @@ rect = [0, 0, 0, 0]
 mode = cv2.GC_EVAL
 bgd_model = np.zeros((1, 65), np.float64)
 fgd_model = np.zeros((1, 65), np.float64)
+
+
+def onMouse(event, x, y, flags, param):
+    global mouse_mode, rect, mask, mode
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if flags <= 1:
+            mode = cv2.GC_INIT_WITH_RECT
+            rect[:2] = x, y
