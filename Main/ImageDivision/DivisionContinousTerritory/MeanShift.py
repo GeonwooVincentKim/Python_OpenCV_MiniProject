6f044f824adf54b67abe27e8932a1c4004cc1f06
kkwnop <kdsnop@gmail.com>
2020-12-30 21:47:28 +0900

    Collect the selected value

--- Main/ImageDivision/DivisionContinousTerritory/MeanShift.py
+++ Main/ImageDivision/DivisionContinousTerritory/MeanShift.py
@@ -3,3 +3,9 @@ import numpy as np
 
 
 img = cv2.imread("../../../img/taekwonv1.jpg")
+
+
+def onChange(x):
+    sp = cv2.getTrackbarPos('sp', 'img')
+    sr = cv2.getTrackbarPos('sr', 'img')
+    lv = cv2.getTrackbarPos('lv', 'img')
