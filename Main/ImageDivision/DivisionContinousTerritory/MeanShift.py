a8df7b38723cdb9a93392a452ce737577318986a
kkwnop <kdsnop@gmail.com>
2020-12-30 21:48:46 +0900

    Apply average-movement-filter

--- Main/ImageDivision/DivisionContinousTerritory/MeanShift.py
+++ Main/ImageDivision/DivisionContinousTerritory/MeanShift.py
@@ -9,3 +9,17 @@ def onChange(x):
     sp = cv2.getTrackbarPos('sp', 'img')
     sr = cv2.getTrackbarPos('sr', 'img')
     lv = cv2.getTrackbarPos('lv', 'img')
+
+    mean = cv2.pyrMeanShiftFiltering(
+        img,
+        sp,
+        sr,
+        None,
+        lv
+    )
+    cv2.imshow(
+        "img",
+        np.hstack(
+            (img, mean)
+        )
+    )
