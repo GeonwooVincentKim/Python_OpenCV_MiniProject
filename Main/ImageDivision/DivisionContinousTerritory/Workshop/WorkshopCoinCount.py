9bd258f0d9875e9c378aed14f7d827f29f2aab5a
kkwnop <kdsnop@gmail.com>
2021-01-11 21:35:11 +0900

    Convert the value

--- Main/ImageDivision/DivisionContinousTerritory/Workshop/WorkshopCoinCount.py
+++ Main/ImageDivision/DivisionContinousTerritory/Workshop/WorkshopCoinCount.py
@@ -10,3 +10,21 @@ mean = cv2.pyrMeanShiftFiltering(img, 20, 20)
 cv2.imshow("mean", mean)
 gray = cv2.cvtColor(mean, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (3, 3), 0)
+
+_, thresh = cv2.threshold(
+    gray,
+    0, 255,
+    cv2.THRESH_BINARY | cv2.THRESH_OTSU
+)
+cv2.imshow("thresh", thresh)
+dst = cv2.distanceTransform(
+    thresh,
+    cv2.DIST_L2,
+    3
+)
+dst = (
+        dst /
+        (dst.max() - dst.min())
+        * 255
+).astype(np.uint8)
+cv2.imshow("dst", dst)
