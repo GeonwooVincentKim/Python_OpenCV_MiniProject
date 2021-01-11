f6bc0d179b94d791e9a90b5001daeafb52acf207
kkwnop <kdsnop@gmail.com>
2021-01-11 21:33:08 +0900

    Apply avarage shift

--- Main/ImageDivision/DivisionContinousTerritory/Workshop/WorkshopCoinCount.py
+++ Main/ImageDivision/DivisionContinousTerritory/Workshop/WorkshopCoinCount.py
@@ -5,3 +5,8 @@ import numpy as np
 img = cv2.imread("../../../../img/coins_connected.jpg")
 rows, cols = img.shape[: 2]
 cv2.imshow("original", img)
+
+mean = cv2.pyrMeanShiftFiltering(img, 20, 20)
+cv2.imshow("mean", mean)
+gray = cv2.cvtColor(mean, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (3, 3), 0)
