982d2af594af8b8fc345de2cfb12cdbeec88aa9b
kkwnop <kdsnop@gmail.com>
2021-01-17 17:28:47 +0900

    Resize 16 * 16 size

--- Main/VideoMatching/FindSimilarPicture/AvgHash.py
+++ Main/VideoMatching/FindSimilarPicture/AvgHash.py
@@ -3,3 +3,9 @@ import cv2
 
 img = cv2.imread("../../../img/pistol.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+gray = cv2.resize(gray, (16, 16))
+avg = gray.mean()
+
+bin = 1 * (gray > avg)
+print(bin)
