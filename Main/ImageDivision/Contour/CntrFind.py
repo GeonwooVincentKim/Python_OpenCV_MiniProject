5d9dc98f98546e59a0197f1fba84a420a118ada4
kkwnop <kdsnop@gmail.com>
2021-01-24 22:42:51 +0900

    Update codes

--- Main/ImageDivision/Contour/CntrFind.py
+++ Main/ImageDivision/Contour/CntrFind.py
@@ -6,16 +6,16 @@ img = cv2.imread("../../../img/shapes.png")
 img2 = img.copy()
 
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, imth_res = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY_INV)
+ret, img_res = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY_INV)
 
 contour, hierarchy = cv2.findContours(
-    imth_res,
+    img_res,
     cv2.RETR_EXTERNAL,
     cv2.CHAIN_APPROX_NONE
 )
 
 contour2, hierarchy = cv2.findContours(
-    imth_res,
+    img_res,
     cv2.RETR_EXTERNAL,
     cv2.CHAIN_APPROX_SIMPLE
 )
