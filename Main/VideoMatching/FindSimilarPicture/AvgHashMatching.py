b2786cd38060c78d47b98deedc71ebec60f8f90a
kkwnop <kdsnop@gmail.com>
2021-01-30 22:48:43 +0900

    Generated `img2ghash` function

--- Main/VideoMatching/FindSimilarPicture/AvgHashMatching.py
+++ Main/VideoMatching/FindSimilarPicture/AvgHashMatching.py
@@ -7,3 +7,11 @@ img = cv2.imread("../../../img/pistol.jpg")
 cv2.imshow("query", img)
 
 search_dir = "../../../img/101_ObjectCategories"
+
+
+def img2hash(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.resize(gray, (16, 16))
+    avg = gray.mean()
+    bi = 1 * (gray > avg)
+    return bi
