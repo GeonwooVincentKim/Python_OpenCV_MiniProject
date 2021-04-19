1db86fa22cdff7cd9e5c9b66bbb8505378d94db2
김건우 <kw.kim@xenoimpact.com>
2021-04-19 21:31:44 +0900

    Rounding Three type matching method

--- Main/VideoMatching/TemplateMatching/TemplateMatching.py
+++ Main/VideoMatching/TemplateMatching/TemplateMatching.py
@@ -3,3 +3,12 @@ import numpy as np
 
 
 img = cv2.imread("../../../img/figures.jpg")
+template = cv2.imread("../../../img/taekwonv1.jpg")
+th, tw = template.shape[:2]
+cv2.imshow("template", template)
+
+methods = [
+    'cv2.TM_CCOEFF_NORMED',
+    'cv2.TM_CCORR_NORMED',
+    'cv2.TM_SQDIFF_NORMED'
+]
