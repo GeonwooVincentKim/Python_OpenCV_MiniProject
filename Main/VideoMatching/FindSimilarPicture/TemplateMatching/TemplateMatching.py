60eefd4839cfeeb8210c98a00d0ad82e69123233
김건우 <kw.kim@xenoimpact.com>
2021-04-20 21:11:24 +0900

    [Update] - Moved `TemplateMatching.py` location to the below of `FindSimilarPicture` file-directory

--- Main/VideoMatching/FindSimilarPicture/TemplateMatching/TemplateMatching.py
+++ Main/VideoMatching/FindSimilarPicture/TemplateMatching/TemplateMatching.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 
 
-img = cv2.imread("../../../img/figures.jpg")
-template = cv2.imread("../../../img/taekwonv1.jpg")
+img = cv2.imread("../../../../img/figures.jpg")
+template = cv2.imread("../../../../img/taekwonv1.jpg")
 th, tw = template.shape[:2]
 cv2.imshow("template", template)
 
