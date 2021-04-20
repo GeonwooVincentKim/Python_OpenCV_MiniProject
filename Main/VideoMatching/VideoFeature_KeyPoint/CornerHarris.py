2cc080b72e649e0b4f6cb1f51d9f0595b8ff2db1
김건우 <kw.kim@xenoimpact.com>
2021-04-20 21:17:55 +0900

    [Feature] - Display into the screen

--- Main/VideoMatching/VideoFeature_KeyPoint/CornerHarris.py
+++ Main/VideoMatching/VideoFeature_KeyPoint/CornerHarris.py
@@ -18,3 +18,16 @@ for x, y in coord:
         1,
         cv2.LINE_AA
     )
+
+corner_form = cv2.normalize(
+    corner,
+    None,
+    0, 255,
+    cv2.NORM_MINMAX,
+    cv2.CV_8U
+)
+corner_norm = cv2.cvtColor(
+    corner_form,
+    cv2.COLOR_GRAY2BGR
+)
+merged = np.hstack((corner_norm, img))
